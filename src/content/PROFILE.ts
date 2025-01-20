a8ff1c98beef97dd37adee4a4c3ecff386b8e30b
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-01-19 21:19:19 -0500

    feat: bio

---
+++ src/content/PROFILE.ts
@@ -0,0 +1,53 @@
+export const PROFILE = {
+  first_name: "Cris",
+  site: {
+    SEO: {
+      //     more complicated description for SEO
+      title: "Cristian Sangucho",
+      short_description:
+        "Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community.",
+    },
+  },
+  name: "Cristian Sangucho",
+  headLine2:
+    "I enjoy providing software solutions and bring ideas to life and solve real-world problems.",
+  website: "",
+  avatarUrl: "",
+  shortIntros: [
+    "💻 Student of Software Engineer | Team Player | Self-starter",
+    // "💻 Student of Software Engineer | Open Source Lover | Self-starter",
+    "☁️ Google Cloud Architect Learning Path in progress",
+    "💡 Inspired by Improving Workflows and Realizing Innovative Ideas",
+    "🌱 Lifelong Learner",
+    "🎥 Photo and video editing | 🐾 Animal care | 🎮 Video games",
+    // "📚 Book reader | 🥏 Frisbee Ultimate Player | 🎲 Board Game Player",
+  ],
+  skills: [
+    "Python / Django",
+    "Google Cloud",
+    "Java | Jakarta EE",
+    "Docker",
+    "React",
+    "BDs SQL ",
+    "Astro",
+  ],
+  links: {
+    github: "",
+    twitter: "",
+    stackoverflow: "",
+    devto: "",
+    linkedin: "",
+    credly: "",
+  },
+  studies: [
+    // studies, qualifications and certifications
+    {
+      title: "Student Software Enginner",
+      level: "Seventh Semester",
+    },
+    {
+      title: "AWS Cloud Professional",
+      institution: "AWS",
+    },
+  ],
+};
